7e9c34b53754bf5a71cc8e474c510f715e738986
JosefK <mail@josefkaeufl.de>
2026-09-24 16:53:06 +0200

    ueberarbeitet

--- slides.js
+++ slides.js
@@ -13,5 +13,9 @@ window.SLIDES_TXT = `
 01-money-system.html        | The Money System
 02-m2-chart.html            | The Cost of Infinite Money
 03-bitcoin-game-theory.html | Bitcoin Game Theory
-04-bitcoin-asset-and-network.html | Monetary Network
+04-value.html                | What Gives Things Value
+05-bitcoin-asset-and-network.html | Monetary Network
+06-gold-and-money.html      | Gold and Money
+07-criminal-use.html        | Pseudonymous, Not Anonymous
+08-energy-myth.html         | Energy, Well Spent
 `;
