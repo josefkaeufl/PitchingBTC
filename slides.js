93a3f000d592e98a25326e702a48934c022f23d0
JosefK <mail@josefkaeufl.de>
2026-09-26 16:54:13 +0200

    remove numbering

--- slides.js
+++ slides.js
@@ -9,13 +9,13 @@
    Leave the first and last line (window.SLIDES_TXT = ` … `;) intact.
    ============================================================ */
 window.SLIDES_TXT = `
-00-cover.html               | Cover
-01-money-system.html        | The Money System
-02-m2-chart.html            | The Cost of Infinite Money
-03-bitcoin-game-theory.html | Bitcoin Game Theory
-04-value.html                | What Gives Things Value
-05-bitcoin-asset-and-network.html | Monetary Network
-06-gold-and-money.html      | Gold and Money
-07-criminal-use.html        | Pseudonymous, Not Anonymous
-08-energy-myth.html         | Energy, Well Spent
+cover.html               | Cover
+money-system.html        | The Money System
+m2-chart.html            | The Cost of Infinite Money
+bitcoin-game-theory.html | Bitcoin Game Theory
+value.html                | What Gives Things Value
+bitcoin-asset-and-network.html | Monetary Network
+gold-and-money.html      | Gold and Money
+criminal-use.html        | Pseudonymous, Not Anonymous
+energy-myth.html         | Energy, Well Spent
 `;
